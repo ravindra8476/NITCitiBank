632fb5639daaa83d9d9fa52c8b9fcc66874f9995
ravindra8476 <56758567+ravindra8476@users.noreply.github.com>
2020-04-11 13:46:49 +0530

    Code changed

--- DemoProj1/welcome.java
+++ DemoProj1/welcome.java
@@ -5,11 +5,9 @@ int a=10;
 float f=a;  
 System.out.println(a);  
 System.out.println(f);  
+System.out.println("Welcome To Java");  
 }}  //simple Example for java variable widening Example
 
-    public static void main(String args[]){  
-     System.out.println("Welcome To Java");  
-    }  
-}  
+    
 //Simple java program
 
