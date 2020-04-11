767e16efc244fefaebae93cd7cba55064cd94e85
ravindra8476 <56758567+ravindra8476@users.noreply.github.com>
2020-04-11 13:34:58 +0530

    java variables widening Example

--- DemoProj1/welcome.java
+++ DemoProj1/welcome.java
@@ -1,5 +1,7 @@
 class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Welcome To Java");  
-    }  
-}  
+public static void main(String[] args){  
+int a=10;  
+float f=a;  
+System.out.println(a);  
+System.out.println(f);  
+}}  
